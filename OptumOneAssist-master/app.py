949f4a15ea8688d92b0af9ea768e13a8f5dfeabf
Padegal <RAJANI.PADEGAL@OPTUM.COM>
2020-08-07 20:51:10 +0530

    test

---
+++ OptumOneAssist-master/app.py
@@ -0,0 +1,24 @@
+import pickle
+from flask import Flask,render_template,url_for,request
+
+# load the model from disk
+clf = pickle.load(open('model.pkl', 'rb'))
+cv=pickle.load(open('fittranform.pkl','rb'))
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+
+@app.route('/predict',methods=['POST'])
+def predict():       
+    if request.method == 'POST':
+        query = request.form['query']
+        data = [query]
+        vect = cv.transform(data).toarray()
+        my_prediction = clf.predict(vect)
+    return render_template('result.html',prediction = my_prediction)
+
+if __name__ == '__main__':
+	app.run(debug=True)
